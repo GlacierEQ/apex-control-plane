c56193307378141aab21aacabea1d79c55421102
casey <GLACIER.EQUILIBRIUM@GMAIL.COM>
2026-08-22 13:35:51 -1000

    harden forensic function search paths

---
+++ db/migrations/20260822_forensic_function_search_path_hardening.sql
@@ -0,0 +1,5 @@
+alter function forensic.reject_mutation()
+  set search_path = forensic, extensions, pg_catalog, pg_temp;
+
+alter function forensic.prepare_custody_event()
+  set search_path = forensic, extensions, pg_catalog, pg_temp;
